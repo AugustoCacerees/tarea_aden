8f42334cd385b69eda0bc20f595e02c06cac546a
Your Name <you@example.com>
2023-09-20 09:04:53 -0300

    second_comit

--- models/programa.py
+++ models/programa.py
@@ -5,19 +5,12 @@ class Programa(models.Model):
     _description = 'Modelo Programa'
     _rec_name = 'nombre'
 
-    #Atributos
-    nombre = fields.Char(string='Nombre') #Tiene una longitud maxima de 50 caracteres
+    nombre = fields.Char(string='Nombre')
     descripcion = fields.Text(string='Descripcion')
 
-    #Funcion
     def get_alumnos_inscritos(self, programa_id):
         inscripciones = self.env['modulo_alumnos.incripcion'].search([('programa_id', '=', programa_id)])
-                     #env: permite acceso a los modelos y registros de la base de datos.
-                     #search: campo "programa_id" coincide con el ID del programa actual (self.id).
-
         alumnos = inscripciones.mapped('alumnos_id')
-                      #mapped: devuelve una lista con los IDs de alumnos (relacionados a través del campo alumno_id) en
-                      #       esas inscripciones. Devuelve objetos
         resp_alumnos = []
 
         for alumno in alumnos:
